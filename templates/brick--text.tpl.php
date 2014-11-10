2eea36445cbca75cd7bee3212e0b4f85e0b835ba
Anton Staroverov <tonystarring@gmail.com>
2014-11-10 19:07:35 +0300

    Removed child bricks from text brick template

--- templates/brick--text.tpl.php
+++ templates/brick--text.tpl.php
@@ -1,9 +1,1 @@
 <?php print render($content['field_text']) ?>
-
-<?php if ($bricks): ?>
-<ul>
-  <?php foreach ($bricks as $brick): ?>
-  <li><?php print render($brick) ?></li>
-  <?php endforeach ?>
-</ul>
-<?php endif ?>
