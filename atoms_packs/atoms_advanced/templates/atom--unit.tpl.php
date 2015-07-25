e9d050d75ba148d86519199401120ad83fc49c4a
Anton Staroverov <by@tonystar.me>
2015-07-25 23:46:36 +0300

    Cleanup

--- atoms_packs/atoms_advanced/templates/atom--unit.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php print $prefix ?>
-<?php print render($content['atoms']) ?>
-<?php print $suffix ?>
--- atoms_packs/atoms_basic/templates/atom--text.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php print $prefix ?>
-<?php print render($content['field_text']) ?>
-<?php print $suffix ?>
