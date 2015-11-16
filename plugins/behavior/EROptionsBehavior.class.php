ecc01b85cdd7ac65c50f3fec82c767b79ca92da0
Anton Staroverov <by@tonystar.me>
2015-11-16 19:45:21 +0300

    Issue #2616420 by tonystar: Allow to set custom CSS class per referenced brick

---
+++ plugins/behavior/EROptionsBehavior.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class EROptionsBehavior extends EntityReference_BehaviorHandler_Abstract {
+
+  public function schema_alter(&$schema, $field) {
+    $schema['columns']['er_options'] = array(
+      'type' => 'blob',
+      'size' => 'big',
+      'description' => 'Serialized data containing the target entity view options.',
+    );
+  }
+
+  public function load($entity_type, $entities, $field, $instances, $langcode, &$items) {
+    foreach ($items as &$by_entity) {
+      foreach ($by_entity as &$item) {
+        $item['er_options'] = unserialize($item['er_options']);
+      }
+    }
+  }
+
+  public function insert($entity_type, $entity, $field, $instance, $langcode, &$items) {
+    foreach ($items as &$item) {
+      $item['er_options'] = serialize($item['er_options']);
+    }
+  }
+
+  public function update($entity_type, $entity, $field, $instance, $langcode, &$items) {
+    foreach ($items as &$item) {
+      $item['er_options'] = serialize($item['er_options']);
+    }
+  }
+
+}
