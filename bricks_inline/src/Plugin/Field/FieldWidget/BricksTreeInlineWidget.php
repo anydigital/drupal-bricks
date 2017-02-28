6767e07fcee6bf5c56490b36abc57941d1f10723
Anton Staroverov <by@tonystar.me>
2017-02-28 21:57:57 +0300

    Initial integration with Inline Entity Form

---
+++ bricks_inline/src/Plugin/Field/FieldWidget/BricksTreeInlineWidget.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\bricks_inline\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\inline_entity_form\Plugin\Field\FieldWidget\InlineEntityFormComplex;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * @FieldWidget(
+ *   id = "bricks_tree_inline",
+ *   label = @Translation("Bricks tree (Inline entity form)"),
+ *   description = @Translation("A tree of inline entity forms."),
+ *   field_types = {
+ *     "bricks"
+ *   },
+ *   multiple_values = true
+ * )
+ */
+class BricksTreeInlineWidget extends InlineEntityFormComplex {
+
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    $element['entities']['#widget'] = 'bricks_tree_inline';
+
+    $entities = $form_state->get(['inline_entity_form', $this->getIefId(), 'entities']);
+    foreach ($entities as $delta => $value) {
+      $element['entities'][$delta]['depth'] = array(
+        '#type' => 'number',
+        '#default_value' => !empty($items[$delta]->depth) ? $items[$delta]->depth : 0,
+        '#weight' => 10,
+        '#attributes' => array(
+          'class' => array('bricks-depth'),
+        ),
+      );
+    }
+
+    return $element;
+  }
+
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    $field_name = $this->fieldDefinition->getName();
+
+    foreach ($values as $delta => $value) {
+      $values[$delta]['depth'] = $form_state->getValue($field_name)['entities'][$delta]['depth'];
+    }
+
+    return $values;
+  }
+
+}
